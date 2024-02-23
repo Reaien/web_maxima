e8edb34b8200bb1826aac27aa4b291e8fd42e83e
Reaien <ivanduarteherrera@gmail.com>
2024-02-23 12:57:24 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="bg-violet-500 hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
+        Hola mundo chuchetumare
+      </button>
+    </>
+  );
+}
+
+export default App;
