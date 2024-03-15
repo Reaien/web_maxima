f05b02bda286ea68c1d38b5e6995ea7e260f40bd
Reaien <ivanduarteherrera@gmail.com>
2024-03-14 23:29:32 -0300

    nuestra mision avance

--- src/components/NuestraMision.jsx
+++ src/components/NuestraMision.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { NavBar } from "./NavBar.jsx";
 import { ToggleMenuXL } from "./ToggleMenuXL.jsx";
+import imagen_mision from "../assets/capa_maxima.webp";
 
 const NuestraMision = () => {
   return (
     <>
       <NavBar />
       <ToggleMenuXL />
-      <section className="grid grid-rows-1 grid-cols-2">
-        <div>div col</div>
-        <div>div col</div>
+      <section className="grid grid-rows-1 grid-cols-2 mt-10 bg-white shadow-md">
+        <div className="mx-auto">
+          <p className="text-5xl font-bold font-overpass mt-5">
+            Nuestra misión
+          </p>
+        </div>
+        <div className="h-[500px] relative">
+          <div className="absolute inset-0 md:bg-gradient-to-l md:from-transparent md:to-white/75"></div>
+          <img
+            className="object-cover w-full h-full"
+            src={imagen_mision}
+            alt="imagen_mision"
+          />
+        </div>
+      </section>
+
+      <section className="grid grid-rows-1 grid-cols-3 items-center text-center justify-center">
+        <div>div1</div>
+        <div>div2</div>
+        <div>div3</div>
       </section>
     </>
   );
